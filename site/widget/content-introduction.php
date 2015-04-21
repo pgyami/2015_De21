54ff6753bb0ef4f2317dc4d875163e9043966246
ngokpika <ngokpika@gmail.com>
2015-04-21 16:03:53 +0700

    Intro + About content

--- site/widget/content-introduction.php
+++ site/widget/content-introduction.php
@@ -1,1 +1,23 @@
-<h1>Introduction hien thi len first page.</h1>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Introduction</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+		<!-- Bootstrap -->
+<link type="text/css" rel="stylesheet" href="../../public/css/bootstrap.css">
+<script src ="../../public/js/bootstrap.js"></script>
+</head>
+<body>       
+    <h1>BSLK website cung cấp điều gì?</h1>
+    <p>BSLK cung cấp Rest API cho phép người dùng kết nối với mọi cơ sở dữ liệu được đăng tải một cách trực quan và nhanh chóng nhất</p>
+    <img src="../../public/images/database-symbol.png" alt="Smiley face" height="150" width="150">
+    <h1>Kết nối tới mọi cơ sở dữ liệu</h1>
+    <p>Cung cấp các giao diện trực quan giúp người sử dụng dễ dàng truy vấn và quản lý hiệu quả với một cơ chế bảo mật an toàn, đáng tin cậy nhất nhằm mang đến cho người dùng sự ân tâm trong quá trình sử dụng.</p>
+    
+</div>
+
+
+ 
+</body>
+</html>
--- site/widget/footer.php
+++ site/widget/footer.php
@@ -1,3 +1,4 @@
             <div class="col-lg-12 footer-below">
                 <h1>©2015 Design by BSLK Production. All Rights Reserved </h1>
+                <h2>Please contact: 01662463882 if you want have any question</h2>
 			</div>
