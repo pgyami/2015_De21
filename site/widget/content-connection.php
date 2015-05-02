ae55ad2aec4a6a3001cddeb4f34a78e4f0b47d69
Phạm Ngọc Khánh <raito_yagami_9x@yahoo.com.vn>
2015-05-03 01:02:36 +0700

    Quản lý db đầy đủ

--- site/widget/content-connection.php
+++ site/widget/content-connection.php
@@ -10,6 +10,24 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 </head>
 <body> 
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['user']) && !empty($_POST['host']) && !empty($_POST['password'])) {
+            $first_name = $_POST['first_name']; 
+            $last_name = $_POST['last_name'];
+            $user = $_POST['user'];
+            $host = $_POST['host'];
+            $password = $_POST['password'];
+            $query = "INSERT INTO userinfo.connection_info(first_name, last_name,user_name, host, password) VALUES ('$first_name','$last_name','user','host','password') ";
+            $result = mysqli_query($dbc, $query);
+            if (mysqli_affected_rows($dbc) == 1){
+                echo "Add user successfully";
+            } else {
+                echo "Error when insert";
+            }
+        }
+    }
+?>
 <div class="panel-group">
       <div class="panel panel-info">
         <div class="panel-heading hover-link" data-toggle="collapse" data-parent="false" data-target="#collapseTableOne">
@@ -35,27 +53,32 @@
                 $query = "SELECT * FROM userinfo.connection_info";
                 $result = mysqli_query($dbc, $query);
                 while ($row = mysqli_fetch_assoc($result)){
-                    echo "<tr>
+                    $id = $row['id'];
+                    echo "<form method='POST' id='frmConnection' name='frmConnection' action='index.php?action=manager_db'><tr>
+                        <input type='hidden' name='id' value='$id'/>
                       <td><input type='checkbox'></td>
                       <td>" . $row['id'] . "</td>
                       <td>" . $row['first_name'] . "</td>
                       <td>" . $row['last_name']. "</td>
                       <td>" . $row['user_name'] . "</td>
+                      
                       <td class='text-right'>
                         <div class='dropdown'>
                           <a data-toggle='dropdown' href='#'>Actions</a>
                           <ul class='dropdown-menu dropdown-menu-right' role='menu' aria-labelledby='dLabel'>
-                            <li><a href='index.php?action=manager_db'>Manager db</a></li>
-                            <li><a href='#'>Different Action</a></li>
-                            <li><a href='#'>Another Option</a></li>
-                            <li><a href='#'>Yet Another Action</a></li>
+                            <li><input type='submit' name='submit' value='Manager this db'></li>
                           </ul>
                         </div>
                     </td>
-                    </tr>";             
+                    </tr></form>";             
                 }
                 ?>
                 <script>
+                    $('#clickme').click(function(){
+                        //$('#frmConnection').submit();
+                        //$(location).attr('http://localhost/2015_De21/index.php?action=manager_db',url);
+                        //window.location.href = "index.php?action=manager_db";
+                    });
                     
                 </script>
               </tbody>
@@ -68,12 +91,19 @@
       <div class="panel panel-success">
         <div class="panel-heading hover-link" data-toggle="collapse" data-parent="false" data-target="#collapseTableTwo">
           <h4 class="panel-title">
-            Create New Connection
+            Create connection
           </h4>
         </div>
         <div id="collapseTableTwo" class="panel-collapse collapse">
           <div class="panel-body">
-            
+            <form method="POST">
+                First name: <input type="input" name="first_name"/><?if ($_SERVER['REQUEST_METHOD'] == 'POST' && in_array("first_name",$errors)) echo "Required" ?><br />
+                Last name: <input type="input" name="last_name"/><?if ($_SERVER['REQUEST_METHOD'] == 'POST' && in_array("last_name",$errors)) echo "Required" ?><br />
+                Username: <input type="input" name="user"/><?if ($_SERVER['REQUEST_METHOD'] == 'POST' && in_array("user",$errors)) echo "Required" ?><br />
+                Host: <input type="input" name="host"/><?if ($_SERVER['REQUEST_METHOD'] == 'POST' && in_array("host",$errors)) echo "Required" ?><br />
+                Password: <input type="input" name="password"/><?if ($_SERVER['REQUEST_METHOD'] == 'POST' && in_array("password",$errors)) echo "Required" ?><br />
+                <input type="submit" value="Add connection"/>
+            </form>
           </div>
         </div>
       </div>
--- site/widget/content-manager-db.php
+++ site/widget/content-manager-db.php
@@ -1,10 +1,28 @@
-<?php 
-$_SESSION['hostname'] = "localhost";
-$_SESSION['username'] = "root";
-$_SESSION['password'] = "123456";
-?> 
-<?php $dbc = mysqli_connect($_SESSION['hostname'],$_SESSION['username'],$_SESSION['password']);?>
+
 <?php
+$dbc = mysqli_connect('localhost','root','123456');
+if (empty($_SESSION['hostname'])){
+    
+    if (!empty($_POST['id'])){
+        $id = $_POST['id'];
+        $query = "SELECT * FROM userinfo.connection_info WHERE id=$id";
+        $result = mysqli_query($dbc, $query);
+        $row = mysqli_fetch_array($result);
+        $_SESSION['hostname'] = $row['host'];
+        $_SESSION['username'] = $row['user_name'];
+        $_SESSION['password'] = $row['password'];
+    } 
+}
+
+?> 
+<?php $dbc = mysqli_connect($_SESSION['hostname'],$_SESSION['username'],$_SESSION['password']);
+if (!$dbc){
+    echo "Connection lost";
+    exit(0);
+}
+?>
+<?php
+    
     if (isset($_GET['selecteddatabase']))
         $selecteddatabase = mysqli_escape_string($dbc, $_GET['selecteddatabase']);
     if (isset($_GET['selectedtable']))
@@ -66,17 +84,17 @@ $_SESSION['password'] = "123456";
                 $dbname = $row['SCHEMA_NAME'];
                 echo "<li><a href='index.php?action=manager_db&selecteddatabase=$dbname'>".$dbname."</a>";
                 if ($dbname == $selecteddatabase){
-                    if (mysqli_num_rows($result1) != 0){
-                        echo "<ul>";
+                    echo "<ul>";
+                        if (mysqli_num_rows($result1) != 0){
                         while ($row = mysqli_fetch_array($result1, MYSQLI_ASSOC)){
                             echo "<li>";
                             $tablename = $row["Tables_in_$selecteddatabase"];
                             echo "<a href='index.php?action=manager_db&selecteddatabase=$dbname&selectedtable=$tablename'>".$tablename."</a>";
                             echo "</li>";
                         }
-                        echo "<li><a href='index.php?action=create_table&selecteddatabase=$dbname'>Create table</a></li>";
-                        echo "</ul>";
                     }
+                    echo "<li><a href='index.php?action=create_table&selecteddatabase=$dbname'>Create table</a></li>";
+                    echo "</ul>";
                 }
                 echo "</li>";
             }
@@ -135,12 +153,80 @@ $_SESSION['password'] = "123456";
             echo "<li><a href='index.php?action=create_database'>Create database</a></li>";
             echo "</ul>";
         }
+        //Kiem tra va them data
+        $query = "DESC $selecteddatabase.$selectedtable";                
+        $result = mysqli_query($dbc, $query);
+        $check = false;
+        echo $query;
+        while ($rows = mysqli_fetch_assoc($result)){
+            $row = $rows['Field'];
+            if (!empty($_POST[$row])){
+                $check = true;
+                break;
+            }            
+        }
+        if ($check == true) {
+            $query = "DESC $selecteddatabase.$selectedtable";                
+            $result = mysqli_query($dbc, $query);
+            $add = true;
+            while ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                $row = $rows['Field'];
+                if (empty($_POST[$row])){
+                    $add = false;
+                    break;
+                }
+            }
+            if ($add == true){
+                $query = "DESC $selecteddatabase.$selectedtable";                
+                $result = mysqli_query($dbc, $query);
+                $add = true;
+                while ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    $row = $rows['Field'];
+                    if (empty($_POST[$row])){
+                        $add = false;
+                        break;
+                    }
+                }
+                $query1 = "INSERT INTO $selecteddatabase.$selectedtable VALUES (";
+                $result = mysqli_query($dbc, $query);
+                while ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                    $row = $rows['Field'];
+                    $query1 = $query1 . "'" . $_POST[$row] . "',";
+                }
+                $query1 = $query1 . ")";
+                $query1 = str_replace(",)",")",$query1);
+                $result = mysqli_query($dbc, $query1);
+                if (mysqli_affected_rows($dbc) == 1){
+                    echo "Them dong thanh cong";
+                } else {
+                    echo "Them dong that bai";
+                }
+            } else {
+                echo "Nhap day du thong tin";
+            }
+        }
         
+        if (!empty($_POST['delete_button'])){
+            $delete_field = $_POST['delete_field'];
+            $delete_value = $_POST['delete_value'];
+            $query = "DELETE FROM $selecteddatabase.$selectedtable WHERE $delete_field='$delete_value'";
+            $result = mysqli_query($dbc, $query);
+            if (mysqli_affected_rows($dbc) == 1) {
+                echo "Xoa dong thanh cong";
+            } else {
+                echo "Xoa dong that bai";
+            }
+        }
+        
+        //hien bang len
         $query = "DESC $selecteddatabase.$selectedtable";                
         $result = mysqli_query($dbc, $query);
         echo "<table><thead><tr>";
+        
+        $delete_field;
         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
             echo "<th>" . $row['Field'] . "</th>";
+            $delete_field = $row['Field'];
         }
         echo "</tr></thead>";
         echo "<tbody>";
@@ -152,16 +238,33 @@ $_SESSION['password'] = "123456";
             for ($i = 0; $i < $num; $i++){
                 echo "<td>" . $row["$i"] . "</td>";
             }
+            $num1 = $num - 1;
+            $delete_value = $row["$num1"];
+            echo "<form method='POST'>";
+            echo "<td><input type='submit' name='delete_button' value='Delete'></td>";
+            echo "<td><input type='hidden' name='delete_field' value='$delete_field'></td>";
+            echo "<td><input type='hidden' name='delete_value' value='$delete_value'></td>";
+            echo "</form>";
             echo "</tr>";
         }
-        //Hang cuoi cung them data
+        
+        
+        //Hien hang cuoi cung
+        echo "<form method='POST'>";
         $query = "DESC $selecteddatabase.$selectedtable";                
         $result = mysqli_query($dbc, $query);
-        echo "<table><thead><tr>";
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            echo "<th>" . $row['Field'] . "</th>";
+        echo "<tr>";
+        while ($rows = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $row = $rows['Field'];
+            echo "<td>" . "<input type='input' name='$row'>" . "</td>";
         }
+        echo "<input type='submit' name='add_row' value='Add row'/>"; 
+        echo "</tr>";
+        
+        echo "</form>";
+        
         echo "</tbody>";
-        echo "</table>";   
+        echo "</table>";  
+        
     }
 ?>
